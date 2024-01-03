6ee5baf1083b81d4b691cd57835299257c0908e7
Ethanm-0371 <ethanm0371@gmail.com>
2024-01-03 19:13:32 +0100

    Encapsulated details screen in stateful widget.
    - The screen itself is no longer a stateful widget.
    - This was done to later implement the layout variation for the display of the GameItem, which is slightly different.

--- lib/screens/set_details_screen.dart
+++ lib/screens/set_details_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:warframemarket_app/model/item_loader.dart';
+import 'package:warframemarket_app/model/set_loader.dart';
 import 'package:warframemarket_app/widgets/background.dart';
 import 'package:warframemarket_app/widgets/custom_button.dart';
 import 'package:warframemarket_app/widgets/item_widgets/item_description.dart';
@@ -7,21 +7,8 @@ import 'package:warframemarket_app/widgets/item_widgets/item_title.dart';
 import 'package:warframemarket_app/widgets/item_widgets/item_values_box.dart';
 import 'package:warframemarket_app/widgets/item_widgets/set_preview.dart';
 
-class SetDetailsScreen extends StatefulWidget {
+class SetDetailsScreen extends StatelessWidget {
   const SetDetailsScreen({super.key});
-
-  @override
-  State<SetDetailsScreen> createState() => _SetDetailsScreenState();
-}
-
-class _SetDetailsScreenState extends State<SetDetailsScreen> {
-  int activeItem = 0;
-
-  void setActiveItem(int itemNum) {
-    setState(() {
-      activeItem = itemNum;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,74 +35,102 @@ class _SetDetailsScreenState extends State<SetDetailsScreen> {
             );
           }
           final dataRequest = snapshot.data!;
-          return Center(
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                const SarynBackground(),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ItemTitle(
-                        itemName: (activeItem == 0)
-                            ? dataRequest.set.itemName.toUpperCase()
-                            : dataRequest.components[activeItem - 1].itemName
-                                .toUpperCase()),
-                    CustomButton(
-                      text: "Wiki",
-                      textSize: 20,
-                      width: 100,
-                      height: 35,
-                      borderSize: 1.5,
-                      imagePath: "assets/lotusSymbol.png",
-                      function: () {
-                        debugPrint("Wiki Button Pressed");
-                      },
-                    ),
-                    SetPreview(
-                        setData: dataRequest,
-                        selectedItem: activeItem,
-                        updateState: setActiveItem),
-                    ItemDescription(
-                        description: (activeItem == 0)
-                            ? dataRequest.set.itemDescription
-                            : dataRequest
-                                .components[activeItem - 1].itemDescription),
-                    ValuesBox(
-                      masteryLevel: (activeItem == 0)
-                          ? dataRequest.set.masteryLevel
-                          : dataRequest.components[activeItem - 1].masteryLevel,
-                      tradingTax: (activeItem == 0)
-                          ? dataRequest.set.tradingTax
-                          : dataRequest.components[activeItem - 1].tradingTax,
-                      ducats: (activeItem == 0)
-                          ? dataRequest.set.ducats
-                          : dataRequest.components[activeItem - 1].ducats,
-                    ),
-                    CustomButton(
-                      text: "LISTINGS",
-                      function: () {
-                        Navigator.pushNamed(context, "/listings",
-                            arguments: (activeItem == 0)
-                                ? dataRequest.set.itemName
-                                : dataRequest
-                                    .components[activeItem - 1].itemName);
-                      },
-                    ),
-                    CustomButton(
-                      text: "SOURCES",
-                      function: (activeItem != 0)
-                          ? () {
-                              Navigator.pushNamed(context, "/sources");
-                            }
-                          : null,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          );
+          return SetDetailsLayout(dataRequest: dataRequest);
         },
+      ),
+    );
+  }
+}
+
+class SetDetailsLayout extends StatefulWidget {
+  const SetDetailsLayout({
+    super.key,
+    required this.dataRequest,
+  });
+
+  final GenericSetData dataRequest;
+
+  @override
+  State<SetDetailsLayout> createState() => _SetDetailsLayoutState();
+}
+
+class _SetDetailsLayoutState extends State<SetDetailsLayout> {
+  int activeItem = 0;
+
+  void setActiveItem(int itemNum) {
+    setState(() {
+      activeItem = itemNum;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          const SarynBackground(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ItemTitle(
+                  itemName: (activeItem == 0)
+                      ? widget.dataRequest.set.itemName.toUpperCase()
+                      : widget.dataRequest.components[activeItem - 1].itemName
+                          .toUpperCase()),
+              CustomButton(
+                text: "Wiki",
+                textSize: 20,
+                width: 100,
+                height: 35,
+                borderSize: 1.5,
+                imagePath: "assets/lotusSymbol.png",
+                function: () {
+                  debugPrint("Wiki Button Pressed");
+                },
+              ),
+              SetPreview(
+                  setData: widget.dataRequest,
+                  selectedItem: activeItem,
+                  updateState: setActiveItem),
+              ItemDescription(
+                  description: (activeItem == 0)
+                      ? widget.dataRequest.set.itemDescription
+                      : widget.dataRequest.components[activeItem - 1]
+                          .itemDescription),
+              ValuesBox(
+                masteryLevel: (activeItem == 0)
+                    ? widget.dataRequest.set.masteryLevel
+                    : widget
+                        .dataRequest.components[activeItem - 1].masteryLevel,
+                tradingTax: (activeItem == 0)
+                    ? widget.dataRequest.set.tradingTax
+                    : widget.dataRequest.components[activeItem - 1].tradingTax,
+                ducats: (activeItem == 0)
+                    ? widget.dataRequest.set.ducats
+                    : widget.dataRequest.components[activeItem - 1].ducats,
+              ),
+              CustomButton(
+                text: "LISTINGS",
+                function: () {
+                  Navigator.pushNamed(context, "/listings",
+                      arguments: (activeItem == 0)
+                          ? widget.dataRequest.set.itemName
+                          : widget
+                              .dataRequest.components[activeItem - 1].itemName);
+                },
+              ),
+              CustomButton(
+                text: "SOURCES",
+                function: (activeItem != 0)
+                    ? () {
+                        Navigator.pushNamed(context, "/sources");
+                      }
+                    : null,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
